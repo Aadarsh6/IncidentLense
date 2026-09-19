ff62a78d1144fcb41827c94d63b4eafe6b1657d6
Adarsh <140302686+Aadarsh6@users.noreply.github.com>
2026-09-19 16:40:25 +0530

    -smaple for demo

---
+++ Frontend/lib/sample.ts
@@ -0,0 +1,11 @@
+export const SAMPLE_LOGS = `2026-09-19T10:41:26Z ==> Cloning from https://github.com/Aadarsh6/X-PostGenerator-Backend
+2026-09-19T10:41:38Z ==> Running build command 'npm install && npx prisma generate && npm run build'...
+2026-09-19T10:41:43Z up to date, audited 272 packages in 4s
+2026-09-19T10:41:44Z Loaded Prisma config from prisma.config.ts.
+2026-09-19T10:41:44Z Prisma schema loaded from prisma/schema.prisma.
+2026-09-19T10:41:44Z ✔ Generated Prisma Client (7.8.0) to ./generated/prisma in 29ms
+2026-09-19T10:41:44Z > x-post-backend@1.0.0 build
+2026-09-19T10:41:44Z > tsc
+2026-09-19T10:41:46Z src/index.ts(19,17): error TS2304: Cannot find name 'apiRoutes'.
+2026-09-19T10:41:46Z ==> Build failed
+2026-09-19T10:41:46Z ==> Common ways to troubleshoot your deploy: https://render.com/docs/troubleshooting-deploys`;
